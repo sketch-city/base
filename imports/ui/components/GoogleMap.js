cc3351946f0ef49d0f63c29a8673e2f571471ab3
Lucas Blancas <lablancas@gmail.com>
2017-09-02 16:00:49 -0500

    remove search box from map

--- imports/ui/components/GoogleMap.js
+++ imports/ui/components/GoogleMap.js
@@ -2,25 +2,9 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 
 import { withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
-import SearchBox from 'react-google-maps/lib/places/SearchBox';
 import withScriptjs from "react-google-maps/lib/async/withScriptjs";
 
 import { MarkerInfo } from './MarkerInfo.js';
-
-const INPUT_STYLE = {
-  boxSizing: 'border-box',
-  MozBoxSizing: 'border-box',
-  border: '1px solid transparent',
-  width: '240px',
-  height: '32px',
-  marginTop: '12px',
-  padding: '0 12px',
-  borderRadius: '1px',
-  boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)',
-  fontSize: '14px',
-  outline: 'none',
-  textOverflow: 'ellipses',
-};
 
 export const SyncGoogleMap = withGoogleMap(
   props => (
@@ -30,14 +14,6 @@ export const SyncGoogleMap = withGoogleMap(
       defaultCenter={{ lat: 29.7615527, lng: -95.3615587 }}
       onClick={props.onMapClick}
     >
-      <SearchBox
-        ref={props.onSearchBoxMounted}
-        bounds={props.bounds}
-        controlPosition={google.maps.ControlPosition.TOP_LEFT}
-        onPlacesChanged={props.onPlacesChanged}
-        inputPlaceholder="Search"
-        inputStyle={INPUT_STYLE}
-      />
       {props.markers.map((marker, index) => (
         <Marker key={index}
           {...marker}
