1a0add3c239c059cbaef79b441cf0b2b4419429c
Lucas Blancas <lablancas@gmail.com>
2017-09-02 16:00:39 -0500

    remove app bar button and unused imports

--- imports/ui/components/AppNavigation.js
+++ imports/ui/components/AppNavigation.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Navbar } from 'react-bootstrap';
-import { Link } from 'react-router';
 import { Meteor } from 'meteor/meteor';
-import PublicNavigation from './PublicNavigation.js';
-import AuthenticatedNavigation from './AuthenticatedNavigation.js';
 import container from '../../modules/container';
 import AppBar from 'material-ui/AppBar';
-import FlatButton from 'material-ui/FlatButton';
-
-const renderNavigation = hasUser => (hasUser ? <AuthenticatedNavigation /> : <FlatButton href="/add" label="Add" />);
 
 const AppNavigation = ({ hasUser }) => (
   <AppBar
     title="Harvey Admin"
     showMenuIconButton={false}
-    iconElementRight={renderNavigation(hasUser)}
   />
 );
 
